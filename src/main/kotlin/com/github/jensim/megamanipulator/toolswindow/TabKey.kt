1bb7372167c3725095e4f0491533c5abeb2aaa16
Jens Brimfors <3663856+jensim@users.noreply.github.com>
2021-10-16 15:37:20 +0200

    Suppress naming

--- src/main/kotlin/com/github/jensim/megamanipulator/toolswindow/TabKey.kt
+++ src/main/kotlin/com/github/jensim/megamanipulator/toolswindow/TabKey.kt
@@ -1,5 +1,6 @@
 package com.github.jensim.megamanipulator.toolswindow
 
+@SuppressWarnings("EnumNaming") // Must match names in message bundle
 enum class TabKey {
     tabTitleSettings,
     tabTitleSearch,
